713242f08a974562a923cdb10c3c2873fc8842e1
Netset Developer <mobileexpert1234@gmail.com>
2019-01-08 18:51:50 +0530

    router change

--- src-demo/nav/router.js
+++ src-demo/nav/router.js
@@ -19,3 +19,8 @@ const AppRoutes = createStackNavigator({
 
 export const AppNav= createAppContainer(AppRoutes);
 export default AppNav;
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /home/netset/milap_projects/Cambuzz_sign_apk/MY-RELEASE-KEY.keystore /home/netset/milap_projects/Cambuzz_sign_apk/app-release-unsigned.apk cambuzz
+
+
+  /home/netset/Android/Sdk/build-tools/26.0.2/zipalign -v 4 /home/netset/milap_projects/Cambuzz_sign_apk/app-release-unsigned.apk /home/netset/milap_projects/Cambuzz_sign_apk/cambuzz_app.apk
